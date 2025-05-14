0f0c5d71a4bb4e25519956e7a14848dfcdacfa24
Enthony Araujo <enthonyaraujo01@gmail.com>
2025-05-14 19:12:18 -0300

    update waybar

--- waybar/scripts/gpu_temp_usage.sh
+++ waybar/scripts/gpu_temp_usage.sh
@@ -3,5 +3,5 @@ TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits)
 
 USAGE=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{print $1}')
 
-echo "GPU: ${USAGE}% ${TEMP}°C"
+echo "GPU: ${USAGE}%   ${TEMP}°C"
 

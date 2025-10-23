d0b7263df4c523121265cc7eeda6b85c9dd95d83
Enthony Araujo <enthonyaraujo01@gmail.com>
2025-10-23 16:30:26 -0300

    adicionei o hypridle | mudancas na waybar

--- waybar/scripts/cpu_usage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-loadavg=$(cut -d " " -f1 /proc/loadavg)
-
-cores=$(nproc)
-
-usage=$(echo "$loadavg $cores" | awk '{ printf "%.0f", ($1 / $2) * 100 }')
-
-echo "{\"text\": \"${usage}%\"}"

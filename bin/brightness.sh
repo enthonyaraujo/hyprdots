18fd77419e29eb372024a99572b778c02ccfa145
Enthony Araujo <enthonyaraujo01@gmail.com>
2025-11-07 11:59:12 -0300

    add: bin

---
+++ bin/brightness.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+step=5  
+
+case "$1" in
+    up)
+        brightnessctl set ${step}%+
+        ;;
+    down)
+        brightnessctl set ${step}%-
+        ;;
+    *)
+        echo "Uso: $0 {up|down}"
+        exit 1
+        ;;
+esac
+
+brightness=$(brightnessctl get)
+max_brightness=$(brightnessctl max)
+percent=$((100 * brightness / max_brightness))
+
+
+if [ "$percent" -lt 30 ]; then
+    icon="󰃞"    
+elif [ "$percent" -lt 70 ]; then
+    icon="󰃟"    
+else
+    icon="󰃠"    
+fi
+
+#notify-send -h int:value:"$percent" \
+            #-h string:x-canonical-private-synchronous:brightness \
+            #-u low "$icon  Brilho" "${percent}%"
+
+dunstify -a "Brilho" \
+         -u low \
+         -r 9999 \
+         -h int:value:"$percent" \
+         "$icon  Brilho" "${percent}%"

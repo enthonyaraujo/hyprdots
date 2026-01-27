60f279c95fe0da0cdd4f9fd9b22dd686912ff15c
Enthony Araujo <enthonyaraujo01@gmail.com>
2026-01-27 16:25:44 -0300

    fix install.sh

--- themes/dark/install.sh
+++ themes/dark/install.sh
@@ -8,13 +8,14 @@ rsync -av rofi $HOME/.config/
 rsync -av waybar $HOME/.config/
 rsync -av .gtkrc-2.0 $HOME/
 
-echo '$wallpaper_hyprland = $HOME/.config/themes/dark/evening-sky.png' > $HOME/.config/hypr/wallpaper.conf
-
-sed -i '2s/.*/   "workbench.colorTheme": "Catppuccin Mocha",/' $HOME/.config/Code/User/settings.json
 
 gsettings set org.gnome.desktop.interface gtk-theme 'catppuccin-mocha-blue-standard+default'
 
-nwg-look -a
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+
+echo '$wallpaper_hyprland = $HOME/.config/themes/dark/evening-sky.png' > $HOME/.config/hypr/wallpaper.conf
+
+sed -i '2s/.*/   "workbench.colorTheme": "Catppuccin Mocha",/' $HOME/.config/Code/User/settings.json
 
 killall waybar && waybar
 killall hyprpaper && hyprpaper &

069ec3d25d75108e2f1f5c742b8c274a7fc02565
Enthony Araujo de Oliveira <enthonyaraujo01@gmail.com>
2026-09-15 20:38:38 -0300

    chore(wallpaper): sincronizar wallpaper shaded_landscape com wallpaper.lua

--- .config/hypr/wallpaper.lua
+++ .config/hypr/wallpaper.lua
@@ -1,4 +1,4 @@
-wallpaper_hyprland = "/home/enthony/.config/wallpapers/yosemite.png"
+wallpaper_hyprland = "/home/enthony/.config/wallpapers/shaded_landscape.png"
 
 return {
     wallpaper_hyprland = wallpaper_hyprland,
